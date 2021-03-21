dc1dfead984eae510aa1131f50ced65cddc388af
Jamal Hasuev <metafra@mail.ru>
2021-03-21 15:51:33 +0300

    init

---
+++ src/Scene.js
@@ -0,0 +1,10 @@
+import config from "./config"
+
+class Scene extends Phaser.Scene {
+  constructor(){
+    super("Game")
+  }
+
+}
+
+export default Scene
---
+++ src/config.js
@@ -0,0 +1,8 @@
+import Scene from "./Scene"
+
+export default {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  scene: new Scene(),
+}
