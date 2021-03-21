a7985a07e0138e654797da13e920cc7443f2505f
Jamal Hasuev <metafra@mail.ru>
2021-03-21 17:40:02 +0300

    add bg + scene refactoring

--- src/Scene.js
+++ src/Scene.js
@@ -1,10 +1,21 @@
 import config from "./config"
 
-class Scene extends Phaser.Scene {
+class StartScene extends Phaser.Scene {
   constructor(){
     super("Game")
   }
 
+  preload() {
+    this.load.image("bg", "assets/img/background.png")
+  }
+  
+  create() {
+    this.createBackground()
+  }
+
+  createBackground() {
+    this.add.sprite(0, 0, "bg").setOrigin(0)
+  }
 }
 
-export default Scene
+export default StartScene
